b54f7e0ea9585e697896c756fbcc7512fd163f02
Michael Trauner <e12019868@student.tuwien.ac.at>
2024-04-08 18:09:33 +0200

    Added implementation of equals and hashCode

--- ass1-jpa/src/main/java/dst/ass1/jpa/model/impl/EmploymentKey.java
+++ ass1-jpa/src/main/java/dst/ass1/jpa/model/impl/EmploymentKey.java
@@ -6,6 +6,7 @@ import dst.ass1.jpa.model.IOrganization;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class EmploymentKey implements IEmploymentKey, Serializable {
@@ -31,5 +32,18 @@ public class EmploymentKey implements IEmploymentKey, Serializable {
 
     public void setOrganization(IOrganization organization) {
         this.organization = (Organization) organization;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmploymentKey that = (EmploymentKey) o;
+        return Objects.equals(driver, that.driver) && Objects.equals(organization, that.organization);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(driver, organization);
     }
 }
